d0f8d8d7065868c7a7d267007184caa9123886f4
stephencruzwright <stephencruzwright@gmail.com>
2026-03-03 21:26:55 +0000

    feat: integrate TouchZone component and enhance GroupifyScreen functionality

--- src/components/groupify/TouchZone.tsx
+++ src/components/groupify/TouchZone.tsx
@@ -1,21 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { View, GestureResponderEvent } from "react-native";
 import { Participant } from "../../types/groupify";
 
 export default function TouchZone({
-    participants,
-    addParticipant,
-    removeParticipant,
-} : {
-    participants: Participant[],
-    addParticipant: () => [],
-    removeParticipant: () => [],
+  participants,
+  addParticipant,
+  removeParticipant,
+}: {
+  participants: Participant[];
+  addParticipant: (participant: Participant) => void;
+  removeParticipant: (id: string) => void;
 }) {
+  const handleGrant = (event: GestureResponderEvent) => {
+    const touches = event.nativeEvent.touches;
+
+    touches.forEach((touch) => {
+      const newParticipant: Participant = {
+        id: String(touch.identifier),
+        x: touch.pageX,
+        y: touch.pageY,
+      };
+      addParticipant(newParticipant);
+    });
+  };
+  const handleRelease = (event: GestureResponderEvent) => {
+    const touches = event.nativeEvent.changedTouches;
+    touches.forEach((touch) => {
+      removeParticipant(String(touch.identifier));
+    });
+  };
+
   return (
-    <View>
-      <Text>TouchZone</Text>
-    </View>
+    <View
+      style={{ flex: 1, backgroundColor: "#000" }}
+      onStartShouldSetResponder={() => true}
+      onResponderGrant={handleGrant}
+      onResponderRelease={handleRelease}
+    />
   );
 }
-
-const styles = StyleSheet.create({});
